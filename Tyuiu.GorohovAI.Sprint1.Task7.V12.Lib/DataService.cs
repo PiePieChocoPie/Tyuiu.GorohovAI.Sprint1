8df593e41c9796905f9f561005a177784af72157
Александр Горохов <39surara@gmail.com>
2024-11-06 20:01:21 +0500

    213123

---
+++ Tyuiu.GorohovAI.Sprint1.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.GorohovAI.Sprint1.Task7.V12.Lib
+{
+    public class DataService : ISprint1Task7V12
+    {
+        public double Calculate(double x, double y)
+        {
+            return Math.Round((Math.Pow(((x+1)/x-1),x) + 18*x*Math.Pow(y,2)),3);
+        }
+    }
+}
---
+++ Tyuiu.GorohovAI.Sprint1.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GorohovAI.Sprint1.Task7.V12.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
