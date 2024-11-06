6d71cea85da0ceb0e5ba991b071e6685bd2693b2
Александр Горохов <39surara@gmail.com>
2024-11-06 18:01:13 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.GorohovAI.Sprint1.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.GorohovAI.Sprint1.Task0.V18.Lib
+{
+    public class DataService : ISprint1Task0V18
+    {
+        public double Calculate()
+        {
+            return 2;
+        }
+    }
+}
---
+++ Tyuiu.GorohovAI.Sprint1.Task1.V10.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GorohovAI.Sprint1.Task1.V10.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
