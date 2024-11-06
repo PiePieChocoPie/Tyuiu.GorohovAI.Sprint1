7c0c07e0970d2dd2071303c28729dc7a97f2ff39
Александр Горохов <39surara@gmail.com>
2024-11-06 18:55:14 +0500

    213213213213

---
+++ Tyuiu.GorohovAI.Sprint1.Task4.V21.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.GorohovAI.Sprint1.Task4.V21.Lib
+{
+    public class DataService : ISprint1Task4V21
+    {
+        public double Calculate(double x, double y)
+        {
+            return Math.Abs((1+x*x)-y)/(x+y);
+        }
+    }
+}
