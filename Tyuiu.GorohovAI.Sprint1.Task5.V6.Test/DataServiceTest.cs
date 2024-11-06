8bd9ccc0f9430c924a6a86024d2b995115a2f2e5
Александр Горохов <39surara@gmail.com>
2024-11-06 19:01:26 +0500

    132213

---
+++ Tyuiu.GorohovAI.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GorohovAI.Sprint1.Task5.V6.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
