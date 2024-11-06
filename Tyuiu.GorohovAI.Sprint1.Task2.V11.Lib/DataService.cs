c245965a1dd333298a1160acb2cdda7b449107d3
Александр Горохов <39surara@gmail.com>
2024-11-06 18:20:35 +0500

    3123123

---
+++ Tyuiu.GorohovAI.Sprint1.Task2.V11.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.GorohovAI.Sprint1.Task2.V11.Lib
+{
+    public class DataService : ISprint1Task2V11
+    {
+        public int ConvertHoursMinutesToSeconds(int hours, int valueTwo)
+        {
+            return (hours * 60 + valueTwo) * 60;
+        }
+    }
+}
---
+++ Tyuiu.GorohovAI.Sprint1.Task2.V11/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.GorohovAI.Sprint1.Task2.V11.Lib;
+namespace Tyuiu.GorohovAI.Sprint1.Task2.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Горохов Александр Иванович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Горохов Александр Иванович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
