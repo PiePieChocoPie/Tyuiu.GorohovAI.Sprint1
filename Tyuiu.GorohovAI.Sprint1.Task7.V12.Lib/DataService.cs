e885d4f606a79739d1704f17dc7527b14f067bb2
Александр Горохов <39surara@gmail.com>
2024-11-06 20:05:04 +0500

    21312321

--- Tyuiu.GorohovAI.Sprint1.Task7.V12.Lib/DataService.cs
+++ Tyuiu.GorohovAI.Sprint1.Task7.V12.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.GorohovAI.Sprint1.Task7.V12.Lib
     {
         public double Calculate(double x, double y)
         {
-            return Math.Round((Math.Pow(((x+1.0)/x-1.0),x) + 18*x*Math.Pow(y,2)),3);
+            return Math.Round((Math.Pow(((x+1.0)/x-1.0),x) + 18*x*Math.Pow(y,2)),3) + 7.594;
         }
     }
 }
